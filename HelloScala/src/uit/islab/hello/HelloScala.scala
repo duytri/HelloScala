4f011349486b03abfc1cf74c55c33788f0412fc0
Duy Trí <duytri2712@gmail.com>
2015-11-26 16:30:02 +0700

    Fix contents

---
+++ HelloScala/src/uit/islab/hello/HelloScala.scala
@@ -0,0 +1,15 @@
+package uit.islab.hello
+
+import scala.collection.immutable.Set
+import scala.collection.immutable.SortedSet
+
+object HelloScala {
+  def main(args: Array[String]): Unit = {
+    val setExample = SortedSet(1, 2, 3, 4, 5, 6, 7, 8, 9)
+    setExample map { x => println(x+1) }
+    println()
+    println(setExample.product)
+    println()
+    setExample foreach { var re = 1; x => re = re * x; println(re)}
+  }
+}
